79ff3043d17ba5b756d2fd03fec8440e5a863cc7
Marcel Mathé <m.mathe@gmx.net>
2021-04-19 20:52:44 +0200

    Advent Of Code 2015 - Day 01
    Problem 1 & 2 solved

---
+++ aoc2015/day01/src/main.rs
@@ -0,0 +1,39 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let path = &args[1];
+
+    let contents = fs::read_to_string(path).expect("Something went wrong reading the file");
+
+    day01p01(&contents);
+    day01p02(&contents);
+}
+
+fn day01p01(contents: &String) {
+    let mut up = 0;
+    let mut down = 0;
+
+    for c in contents.chars() {
+        if c == '(' { up = up + 1; }
+        if c == ')' { down = down + 1; }
+    }
+
+    println!("Santa has to go to the {} floor.", up - down);
+}
+
+fn day01p02(contents: &String) {
+    let mut position = 0;
+    let mut floor = 0;
+
+    for c in contents.chars() {
+        position = position + 1;
+        if c == '(' { floor = floor + 1; }
+        if c == ')' { floor = floor - 1; }
+
+        if floor == -1 { break; }
+    }
+
+    println!("Santa steps into the basement at position {}", position);
+}
