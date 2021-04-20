e0df63fc3197a137b3e939ad3505ce8a7d501ec0
Marcel Mathé <m.mathe@gmx.net>
2021-04-20 17:04:37 +0200

    Walker Function extracted

--- aoc2015/day03/src/main.rs
+++ aoc2015/day03/src/main.rs
@@ -4,6 +4,7 @@ use std::env;
 use std::fs;
 use std::iter::Iterator;
 use std::sync::mpsc;
+use std::sync::mpsc::{Sender, Receiver};
 use std::thread;
 
 fn main() {
@@ -42,48 +43,37 @@ fn day03p01(contents: &String) {
 }
 
 fn day03p02(contents: &String) {
-    let (tx_santa, rx) = mpsc::channel();
-    let tx_robot = tx_santa.clone();
-    let start = House { x: 0, y: 0 };
+    let (santa_tx, rx): (Sender<House>, Receiver<House>) = mpsc::channel();
+    let robot_tx = santa_tx.clone();
+    let starting_house = House { x: 0, y: 0 };
     let santa_list: Vec<char> = contents.chars().step_by(2).collect();
     let robot_list: Vec<char> = contents.chars().skip(1).step_by(2).collect();
     let mut visited_houses: HashSet<House> = HashSet::new();
+    
+    visited_houses.insert(starting_house);
 
-    visited_houses.insert(start);
-
-    thread::spawn(move || {
-        let mut current_santa_house = start;
-
-        for direction in santa_list {
-            match direction {
-                '^' => current_santa_house.y += 1,
-                'v' => current_santa_house.y -= 1,
-                '>' => current_santa_house.x += 1,
-                '<' => current_santa_house.x -= 1,
-                _ => continue,
-            }
-            tx_santa.send(current_santa_house).unwrap();
-        }
-    });
-
-    thread::spawn(move || {
-        let mut current_robot_house = start;
-
-        for direction in robot_list {
-            match direction {
-                '^' => current_robot_house.y += 1,
-                'v' => current_robot_house.y -= 1,
-                '>' => current_robot_house.x += 1,
-                '<' => current_robot_house.x -= 1,
-                _ => continue,
-            }
-            tx_robot.send(current_robot_house).unwrap();
-        }
-    });
-
+    thread::spawn(move || walker(starting_house, santa_list, santa_tx));
+    thread::spawn(move || walker(starting_house, robot_list, robot_tx));
+    
     for received in rx {
         visited_houses.insert(received);
     }
 
     println!("Number of visited houses: {}", visited_houses.len());
+}
+
+fn walker (starting_house: House, directions: Vec<char>, sender: Sender<House>) {
+    let mut current_house = starting_house;
+
+    for direction in directions {
+        match direction {
+            '^' => current_house.y += 1,
+            'v' => current_house.y -= 1,
+            '>' => current_house.x += 1,
+            '<' => current_house.x -= 1,
+            _ => continue,
+        }
+
+        sender.send(current_house).unwrap();
+    }
 }
