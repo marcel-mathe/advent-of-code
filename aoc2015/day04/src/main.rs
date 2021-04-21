fdabee0e1efcd014bea5078864aaeec7ce290845
Marcel Mathé <m.mathe@gmx.net>
2021-04-21 11:04:33 +0200

    day 4 completed

--- aoc2015/day04/src/main.rs
+++ aoc2015/day04/src/main.rs
@@ -23,4 +23,17 @@ fn day04p01(secret_key: &String) {
     }
 }
 
-fn day04p02(_secret_key: &String) {}
+fn day04p02(secret_key: &String) {
+    let mut md5hasher = Md5::new();
+
+    for n in 117946..10000001 {
+        md5hasher.input_str(&format!("{}{}", &secret_key, n.to_string()));
+        let hash = md5hasher.result_str();
+        md5hasher.reset();
+
+        if hash.starts_with("000000") {
+            println!("md5 for {} is {}", n, hash);
+            break;
+        }
+    }
+}
