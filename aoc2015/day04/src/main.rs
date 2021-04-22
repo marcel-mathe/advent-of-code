1875ca007f856d9a3d6e4bd9bdfa159d141a9736
Marcel Mathé <m.mathe@gmx.net>
2021-04-22 18:46:38 +0200

    Day 4 parallelized w/ Rayon

--- aoc2015/day04/src/main.rs
+++ aoc2015/day04/src/main.rs
@@ -1,39 +1,22 @@
 use crypto::digest::Digest;
 use crypto::md5::Md5;
+use rayon::prelude::*;
 
 fn main() {
     let secret_key = String::from("ckczppom");
 
-    day04p01(&secret_key);
-    day04p02(&secret_key);
+    search(&secret_key, 1..1000001, "00000");
+    search(&secret_key, 1..10000001, "000000");
 }
 
-fn day04p01(secret_key: &String) {
-    let mut md5hasher = Md5::new();
-
-    for n in 1..1000001 {
+fn search(secret_key: &str, range: std::ops::Range<i32>, pattern: &str) {
+    range.into_par_iter().for_each(|n: i32| {
+        let mut md5hasher = Md5::new();
         md5hasher.input_str(&format!("{}{}", &secret_key, n.to_string()));
         let hash = md5hasher.result_str();
-        md5hasher.reset();
 
-        if hash.starts_with("00000") {
+        if hash.starts_with(pattern) {
             println!("md5 for {} is {}", n, hash);
-            break;
         }
-    }
-}
-
-fn day04p02(secret_key: &String) {
-    let mut md5hasher = Md5::new();
-
-    for n in 117946..10000001 {
-        md5hasher.input_str(&format!("{}{}", &secret_key, n.to_string()));
-        let hash = md5hasher.result_str();
-        md5hasher.reset();
-
-        if hash.starts_with("000000") {
-            println!("md5 for {} is {}", n, hash);
-            break;
-        }
-    }
+    });
 }
