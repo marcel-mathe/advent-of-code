3e3ff7148d31df2770db12f6dc4022a85766e951
Marcel Mathé <m.mathe@gmx.net>
2021-04-26 21:15:19 +0200

    Day 6 completed

--- aoc2015/day06/src/main.rs
+++ aoc2015/day06/src/main.rs
@@ -10,7 +10,8 @@ fn main() {
     let contents = fs::read_to_string(path).expect("Something went wrong reading the file");
 
     day06p01(contents.to_string(), 1000);
-    day06p02(contents.to_string());
+    println!();
+    day06p02(contents.to_string(), 1000);
 }
 
 fn day06p01(contents: String, n: i32) {
@@ -64,4 +65,36 @@ fn xy2lamp(n: i32, x1: i32, y1: i32, x2: i32, y2: i32) -> Vec<usize> {
     return result;
 }
 
-fn day06p02(_contents: String) {}
+fn day06p02(contents: String, n: i32) {
+    let re = Regex::new(r"(\w+) (\d+),(\d+) through (\d+),(\d+)").unwrap();
+    let size = (n * n) as usize;
+    let mut v: Vec<u32> = vec![0; size];
+    
+    for line in contents.lines() {
+        let captures = re.captures(line).unwrap();
+
+        let command: String = captures[1].to_string();
+        let start_x: i32 = captures[2].parse().unwrap();
+        let start_y: i32 = captures[3].parse().unwrap();
+        let end_x: i32 = captures[4].parse().unwrap();
+        let end_y: i32 = captures[5].parse().unwrap();
+
+        let lamps = xy2lamp(n, start_x, start_y, end_x, end_y);
+
+        for lamp in lamps.iter() {
+            match command.as_ref() {
+                "toggle" => v[*lamp] += 2,
+                "on" => v[*lamp] +=1,
+                "off" => {
+                    if v[*lamp] >= 1 {
+                        v[*lamp] -= 1;
+                    }
+                },
+                _ => {}
+            }
+        }
+    }
+
+    println!("Total lamps:      {}", v.iter().count());
+    println!("Total brightness: {}", v.iter().sum::<u32>());
+}
