0d30f82c89623b14ff76522624464892af86cc00
Marcel Mathé <m.mathe@gmx.net>
2021-04-27 14:19:13 +0200

    Day 6, update function extracted

--- aoc2015/day06/src/main.rs
+++ aoc2015/day06/src/main.rs
@@ -1,4 +1,3 @@
-use bit_vec::BitVec;
 use regex::Regex;
 use std::env;
 use std::fs;
@@ -9,15 +8,15 @@ fn main() {
 
     let contents = fs::read_to_string(path).expect("Something went wrong reading the file");
 
-    day06p01(contents.to_string(), 1000);
+    day06p01(contents.to_string(), 1000, update_bool_lamp);
     println!();
-    day06p02(contents.to_string(), 1000);
+    day06p02(contents.to_string(), 1000, update_int_lamp);
 }
 
-fn day06p01(contents: String, n: i32) {
+fn day06p01(contents: String, n: i32, update: fn(&str, bool) -> bool) {
     let re = Regex::new(r"(\w+) (\d+),(\d+) through (\d+),(\d+)").unwrap();
     let size = (n * n) as usize;
-    let mut bv = BitVec::from_elem(size, false);
+    let mut vec = vec![false; size];
 
     for line in contents.lines() {
         let captures = re.captures(line).unwrap();
@@ -31,24 +30,37 @@ fn day06p01(contents: String, n: i32) {
         let lamps = xy2lamp(n, start_x, start_y, end_x, end_y);
 
         for lamp in lamps.iter() {
-            match command.as_ref() {
-                "toggle" => {
-                    if bv[*lamp] {
-                        bv.set(*lamp, false);
-                    } else {
-                        bv.set(*lamp, true);
-                    }
-                }
-                "on" => bv.set(*lamp, true),
-                "off" => bv.set(*lamp, false),
-                _ => {}
-            }
+            vec[*lamp] = update(command.as_ref(), vec[*lamp]);
         }
     }
 
-    println!("   Total lamps: {}", bv.iter().count());
-    println!(" Enabled lamps: {}", bv.iter().filter(|x| *x).count());
-    println!("Disabled lamps: {}", bv.iter().filter(|x| !*x).count());
+    println!("   Total lamps: {}", vec.iter().count());
+    println!(" Enabled lamps: {}", vec.iter().filter(|x| **x).count());
+    println!("Disabled lamps: {}", vec.iter().filter(|x| !**x).count());
+}
+
+fn day06p02(contents: String, n: i32, update: fn(&str, u32) -> u32) {
+    let re = Regex::new(r"(\w+) (\d+),(\d+) through (\d+),(\d+)").unwrap();
+    let size = (n * n) as usize;
+    let mut v: Vec<u32> = vec![0; size];
+    for line in contents.lines() {
+        let captures = re.captures(line).unwrap();
+
+        let command: String = captures[1].to_string();
+        let start_x: i32 = captures[2].parse().unwrap();
+        let start_y: i32 = captures[3].parse().unwrap();
+        let end_x: i32 = captures[4].parse().unwrap();
+        let end_y: i32 = captures[5].parse().unwrap();
+
+        let lamps = xy2lamp(n, start_x, start_y, end_x, end_y);
+
+        for lamp in lamps.iter() {
+            v[*lamp] = update(command.as_ref(), v[*lamp]);
+        }
+    }
+
+    println!("Total lamps:      {}", v.iter().count());
+    println!("Total brightness: {}", v.iter().sum::<u32>());
 }
 
 fn xy2lamp(n: i32, x1: i32, y1: i32, x2: i32, y2: i32) -> Vec<usize> {
@@ -65,36 +77,32 @@ fn xy2lamp(n: i32, x1: i32, y1: i32, x2: i32, y2: i32) -> Vec<usize> {
     return result;
 }
 
-fn day06p02(contents: String, n: i32) {
-    let re = Regex::new(r"(\w+) (\d+),(\d+) through (\d+),(\d+)").unwrap();
-    let size = (n * n) as usize;
-    let mut v: Vec<u32> = vec![0; size];
-    
-    for line in contents.lines() {
-        let captures = re.captures(line).unwrap();
-
-        let command: String = captures[1].to_string();
-        let start_x: i32 = captures[2].parse().unwrap();
-        let start_y: i32 = captures[3].parse().unwrap();
-        let end_x: i32 = captures[4].parse().unwrap();
-        let end_y: i32 = captures[5].parse().unwrap();
-
-        let lamps = xy2lamp(n, start_x, start_y, end_x, end_y);
-
-        for lamp in lamps.iter() {
-            match command.as_ref() {
-                "toggle" => v[*lamp] += 2,
-                "on" => v[*lamp] +=1,
-                "off" => {
-                    if v[*lamp] >= 1 {
-                        v[*lamp] -= 1;
-                    }
-                },
-                _ => {}
+fn update_int_lamp(command: &str, v: u32) -> u32 {
+    match command.as_ref() {
+        "toggle" => v + 2,
+        "on" => v + 1,
+        "off" => {
+            if v >= 1 {
+                v - 1
+            } else {
+                v
             }
         }
+        _ => v,
     }
+}
 
-    println!("Total lamps:      {}", v.iter().count());
-    println!("Total brightness: {}", v.iter().sum::<u32>());
+fn update_bool_lamp(command: &str, v: bool) -> bool {
+    match command.as_ref() {
+        "toggle" => {
+            if v {
+                false
+            } else {
+                true
+            }
+        }
+        "on" => true,
+        "off" => false,
+        _ => v,
+    }
 }
