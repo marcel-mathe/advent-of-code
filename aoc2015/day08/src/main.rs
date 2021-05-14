93307283dc2f028fbe6160afdad7c5741aaf3bc1
Marcel Mathé <m.mathe@gmx.net>
2021-05-15 01:30:14 +0200

    Day 8 completed

--- aoc2015/day08/src/main.rs
+++ aoc2015/day08/src/main.rs
@@ -9,7 +9,8 @@ fn main() {
     let contents = fs::read_to_string(path).expect("Something went wrong reading the file");
 
     day08p01(&contents);
-    day08p02(&contents);
+    println!("");
+    day08p02(&contents, 6202);
 }
 
 fn unescape(s: &str) -> Option<String> {
@@ -42,12 +43,14 @@ fn unescape(s: &str) -> Option<String> {
  * after removing any quoting
  **/
 fn count_line(line: &str) -> i32 {
-    return line
+    let f = line
         .strip_prefix("\"")
         .and_then(|s| s.strip_suffix("\""))
         .and_then(|s| unescape(&s))
         .map(|s| s.len() as i32)
         .unwrap_or(0);
+
+    return f;
 }
 
 fn day08p01(contents: &String) {
@@ -61,4 +64,33 @@ fn day08p01(contents: &String) {
     println!("Code - Memory: {}", code_count - memory_count);
 }
 
-fn day08p02(_contents: &String) {}
+fn escape(c: &str) -> &str {
+    match c {
+        "\\" => return "\\\\",
+        "\"" => return "\\\"",
+        _ => return c,
+    }
+}
+
+fn count_escaped_line(line: &str) -> i32 {
+    let mut s = String::new();
+    s.push('"');
+
+    line.chars().for_each(|c| {
+        s.push_str(escape(&c.to_string()));
+    });
+
+    s.push('"');
+
+    return s.len() as i32;
+}
+
+fn day08p02(contents: &String, prev: i32) {
+    let new_count = contents
+        .lines()
+        .fold(0, |acc, x| acc + count_escaped_line(x));
+
+    println!("New String: {}", new_count);
+    println!("Previous String: {}", prev);
+    println!("Difference: {}", new_count - prev);
+}
