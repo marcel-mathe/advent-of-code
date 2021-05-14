d7b1135766d235267ce37d88787605b8b7df7276
Marcel Mathé <m.mathe@gmx.net>
2021-05-14 23:32:52 +0200

    Simplified count_line

--- aoc2015/day08/src/main.rs
+++ aoc2015/day08/src/main.rs
@@ -42,15 +42,12 @@ fn unescape(s: &str) -> Option<String> {
  * after removing any quoting
  **/
 fn count_line(line: &str) -> i32 {
-    let f = line
+    return line
         .strip_prefix("\"")
         .and_then(|s| s.strip_suffix("\""))
-        .and_then(|s| unescape(&s));
-
-    match f {
-        None => 0,
-        Some(s) => s.len() as i32,
-    }
+        .and_then(|s| unescape(&s))
+        .map(|s| s.len() as i32)
+        .unwrap_or(0);
 }
 
 fn day08p01(contents: &String) {
