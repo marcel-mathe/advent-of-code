ad5a484faed201b780180908ac23fa8f98729a69
Marcel Mathé <m.mathe@gmx.net>
2021-12-05 19:02:15 +0100

    Parser extracted

---
+++ aoc2016/day01/Parser.hs
@@ -0,0 +1,70 @@
+module Parser where
+
+import Data.List
+import Text.ParserCombinators.ReadP
+
+-- a point on a grid
+data Point = Point {x :: Integer, y :: Integer}
+    deriving (Eq, Ord, Show)
+
+-- the direction a person looks
+data Direction = N | S | E | W
+    deriving (Show)
+
+-- a person, standing at location, looking in direction
+data Person = Person {
+    location :: Point,
+    direction :: Direction,
+    visited :: [Point] }
+    deriving (Show)
+
+-- rotate left or right
+data Rotation = L | R
+    deriving (Show)
+
+-- walk steps unit in the given direction
+type Steps = Integer
+
+-- a complete move, consisting of a rotation and a number of steps
+data Move = Move { rotation :: Rotation, steps :: Steps }
+    deriving (Show)
+
+-- parse a comma
+commaP :: ReadP Char
+commaP = char ','
+
+-- parse a rotation, L|R
+rotationP :: ReadP Rotation
+rotationP = do
+    rot <- choice [char c | c <- "LR"]
+    case rot of
+        'L' -> return L
+        'R' -> return R
+
+-- parse a digit
+digitP :: ReadP Char
+digitP = satisfy (\char -> char >= '0' && char <= '9')
+
+-- parse a step, many digits
+stepP :: ReadP Integer
+stepP = do 
+    parse <- many1 digitP
+    return (read parse)
+
+-- parse a single move
+moveP :: ReadP Move
+moveP = do
+    skipMany commaP             -- comma, optional
+    skipSpaces                  -- as the name says
+    rotation <- rotationP       -- rotation, L | R
+    steps <- stepP              -- steps, digits, at least one
+    return (Move rotation steps)
+
+-- parse a list of moves
+movesP :: ReadP [Move]
+movesP = many1 moveP
+
+-- parse a list of moves and get the result,
+-- the first of the pair of the last list entry
+parseMoves :: String -> [Move]
+parseMoves s = fst $ head $ reverse $ readP_to_S movesP s
