28b5497b659c9f7d641b02af802bb05df53b929e
Marcel Mathé <m.mathe@gmx.net>
2021-12-06 16:40:40 +0100

    unneccessary state removed

--- aoc2016/day02/Types.hs
+++ aoc2016/day02/Types.hs
@@ -1,12 +1,12 @@
 module Types where
 
--- a state per number on the numpad
+-- a state per button on the numpad
 data NumpadState = Q1 | Q2 | Q3 | Q4 | Q5 | Q6 | Q7 | Q8 | Q9 | QA | QB | QC | QD
     deriving (Show, Eq)
 
 -- the state change events
--- Up, Down, Left, Right, Print, Null
-data NumpadEvent = U | D | L | R | P | N
+-- Up, Down, Left, Right, Print
+data NumpadEvent = U | D | L | R | P
     deriving (Show, Eq)
 
 -- finite state machine type alias
