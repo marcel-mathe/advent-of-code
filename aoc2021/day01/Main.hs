033056f6289848411b27b3acbddbf9591e9a0aa4
Marcel Mathé <m.mathe@gmx.net>
2021-12-12 13:54:34 +0100

    formatting changed

--- aoc2021/day01/Main.hs
+++ aoc2021/day01/Main.hs
@@ -13,21 +13,21 @@ prep s = map (\x -> read x :: Int) (lines s)
 -- example problem, solutions: 7, 5
 example :: IO ()
 example = do
-    let xs = "199\n200\n208\n210\n200\n207\n240\n269\n260\n263\n"
-    putStrLn $ "Example (Part 1): " ++ (show $ compute 1 $ prep xs)
-    putStrLn $ "Example (Part 2): " ++ (show $ compute 3 $ prep xs)
+  let xs = "199\n200\n208\n210\n200\n207\n240\n269\n260\n263\n"
+  putStrLn $ "Example (Part 1): " ++ show (compute 1 $ prep xs)
+  putStrLn $ "Example (Part 2): " ++ show (compute 3 $ prep xs)
 
 problem1 :: IO ()
 problem1 = do
-    xs <- readFile "input/input.txt"
-    putStrLn $ "Part 1: " ++ (show $ compute 1 $ prep xs)
+  xs <- readFile "input/input.txt"
+  putStrLn $ "Part 1: " ++ show (compute 1 $ prep xs)
 
 problem2 :: IO ()
 problem2 = do
-    xs <- readFile "input/input.txt"
-    putStrLn $ "Part 2: " ++ (show $ compute 3 $ prep xs)
+  xs <- readFile "input/input.txt"
+  putStrLn $ "Part 2: " ++ show (compute 3 $ prep xs)
 
 main :: IO ()
 main = do
-    problem1
-    problem2
+  problem1
+  problem2
