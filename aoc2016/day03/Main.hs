a39cbdb6c7ebc83032e16fad474daa58060550fe
Marcel Mathé <m.mathe@gmx.net>
2022-01-03 14:18:42 +0100

    day 3 a little parallized

--- aoc2016/day03/Main.hs
+++ aoc2016/day03/Main.hs
@@ -1,21 +1,21 @@
 module Main where
 
-import System.IO
+import Control.Parallel.Strategies
 import Data.List
-
 import Parser
+import System.IO
 
 -- a valid triangle is a triangle where the length of
 -- two sides is bigger than the third
 validTriangle :: Triangle -> Bool
 validTriangle t = all triangleSum (permutations $ triangleToList t)
-    where
-        triangleToList t = head $ [[x, y, z] | (x, y, z) <- [t]]
-        triangleSum t = (t !! 0) + (t !! 1) > (t !! 2)
+  where
+    triangleToList t = head $ [[x, y, z] | (x, y, z) <- [t]]
+    triangleSum t = (t !! 0) + (t !! 1) > (t !! 2)
 
 -- check a list of triangles
 checkTriangles :: [Triangle] -> [Bool]
-checkTriangles = map validTriangle
+checkTriangles = parMap rpar validTriangle
 
 -- count True in list
 countTrue :: [Bool] -> Int
@@ -23,17 +23,17 @@ countTrue = length . filter id
 
 problem1 :: IO ()
 problem1 = do
-    putStr "Problem 1: "
-    content <- readFile "input/input.txt"
-    print (countTrue $ checkTriangles $ parseTriangles content)
+  putStr "Problem 1: "
+  content <- readFile "input/input.txt"
+  print (countTrue $ checkTriangles $ parseTriangles content)
 
 problem2 :: IO ()
 problem2 = do
-    putStr "Problem 2: "
-    content <- readFile "input/input.txt"
-    print (countTrue $ checkTriangles $ parseTriangleColumns content)
+  putStr "Problem 2: "
+  content <- readFile "input/input.txt"
+  print (countTrue $ checkTriangles $ parseTriangleColumns content)
 
 main :: IO ()
 main = do
-    problem1
-    problem2
+  problem1
+  problem2
